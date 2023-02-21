6918d7e700647f55735db1000827256b0579375c
Amo-Zeng <51903518+Amo-Zeng@users.noreply.github.com>
2023-02-21 02:48:36 +0000

    Pending changes exported from your codespace

---
+++ 手动运行setpython3.7.2.sh
@@ -0,0 +1,4 @@
+#export PATH=/workspaces/ziranpai/Python-3.7.2/bin:$PATH
+sudo rm /usr/bin/python3
+sudo ln -s /workspaces/ziranpai/Python-3.7.2/bin/python3 /usr/bin/python3
+export PATH=/usr/bin:$PATH
